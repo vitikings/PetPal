8f5cce981db7a897c849f4da09bee8f016c12879
Alex Cioarec <cioarec.alexandru96@gmail.com>
2024-04-20 02:07:07 +0300

    Add empty auth pages

---
+++ web/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div>Welcome to PetPal</div>
+    <div>
+      <NuxtLink to="/login">Login</NuxtLink>
+      <NuxtLink to="/register">Register</NuxtLink>
+    </div>
+  </div>
+</template>
